ffb6319f2ea3c1f7fd9559b2af29c62928324e57
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-04-24 10:59:20 +0300

    chore(Setup flask project): Install pip, virtualenv and flask
    [Delivers #157029951]

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+
+"""Load the views- import the views down here to prevent circular import problems"""
+from app import views
+
+"""Load the config file"""
+app.config.from_object(Config)
