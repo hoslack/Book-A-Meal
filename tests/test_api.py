600d12ca7db44f79350ac10b56270e10b75ebb2c
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-04-24 15:55:07 +0300

    chore(Add tests):Added test for add meals endpoint
    [Delivers #157038506]

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,6 +8,7 @@ class TestBookAMealAPI(unittest.TestCase):
         """Initializing a test client and making the environment a testing one"""
         self.app = app.test_client()
         self.app.testing = True
+        self.meal = {'name': 'ugali', 'price': 100}
 
     def sign_in(self, email='user@gmail.com', password='testpass'):
         user_data = {
@@ -44,3 +45,21 @@ class TestBookAMealAPI(unittest.TestCase):
     def test_get_all_meals_has_json(self):
         result = self.app.get('/meals')
         self.assertEqual(result.content_type, 'application/json')
+
+    def test_add_meal_status_code(self):
+        result = self.app.post('/meals', data=self.meal)
+        self.assertEqual(result.status_code, 201)
+
+    def test_add_meal_success_response(self):
+        result = self.app.post('/meals', data=self.meal)
+        self.assertIn('Success', result)
+
+    def test_add_meal_without_data(self):
+        result = self.app.post('/meals')
+        self.assertNotEqual(result.status_code, 201)
+
+    def test_duplicate_meal_creation(self):
+        self.app.post('/meals', self.meal)
+        result1 = self.app.post('/meals', self.meal)
+        self.assertEqual(result1.status_code, 409)
+
