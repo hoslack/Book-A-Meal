ff168ed2d88e715ddacc56c0c20f468d2a27e9a3
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-04-24 15:31:54 +0300

    chore(Add tests):Added test for status and datatype for meals endpoint
    [Delivers #157038506]

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,6 +9,38 @@ class TestBookAMealAPI(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
+    def sign_in(self, email='user@gmail.com', password='testpass'):
+        user_data = {
+            'email': email,
+            'password': password
+        }
+        return self.app.post('/auth/signup', data=user_data)
+
+    def log_in(self, email='user@gmail.com', password='testpass'):
+        user_data = {
+            'email': email,
+            'password': password
+        }
+        return self.app.post('/auth/login', data=user_data)
+
     def test_home_status_code(self):
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
+
+    def test_signin_status_code(self):
+        result = self.sign_in()
+        self.assertEqual(result.status_code, 200)
+
+    def test_login_status_code(self):
+        """test login after signing in"""
+        self.sign_in()
+        result = self.log_in()
+        self.assertEqual(result.status_code, 200)
+
+    def test_get_all_meals_status_code(self):
+        result = self.app.get('/meals')
+        self.assertEqual(result.status_code, 200)
+
+    def test_get_all_meals_has_json(self):
+        result = self.app.get('/meals')
+        self.assertEqual(result.content_type, 'application/json')
